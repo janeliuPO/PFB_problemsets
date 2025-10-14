bb75cd142b10cb2f084eff237bd4878def927945
Jane M. Liu <jane.liu@pomona.edu>
2025-10-14 11:45:45 -0400

    used f-strings and added sys.argv

--- about_me.py
+++ about_me.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-name = "Jane"
-print(name)
+import sys
+
+name = sys.argv[1] #this is a name variable
+
+favecolor = sys.argv[2] #this is a color variable
+
+faveact = sys.argv[3] #this is activity variable
+
+faveanimal = sys.argv[4] #this is the animal variable
+
+print(f"""My name: {name}
+      My favorite color: {favecolor}
+      My favorite activity: {faveact}
+      My favoriate animal: {faveanimal}""")
+
+#if you use the triple quotes then line breaks in the script are read as line breaks!
