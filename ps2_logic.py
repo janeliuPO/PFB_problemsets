093bf309af505a267bb814c705b2c6288f803ff5
Jane M. Liu <jane.liu@pomona.edu>
2025-10-14 15:41:43 -0400

    added nested if/else

--- ps2_logic.py
+++ ps2_logic.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 import sys
-count = 656
-if count > 500:
-    print(count, "Wow. That's a big number")
-else:
-    print(count, "nothing to worry about here")
+
+count = int(sys.argv[1]) #allows me to add number to command line and convet that to an intenger that can be compared
+
+if count > 0 and count < 50: #positive number
+    if count%2 == 0:
+        print("it is an even number that is smaler than 50")
+    else:
+        print("positive, odd")
+
+elif count > 0 and count > 50: #positive number
+    if count%3 == 0:
+        print("it is a number divisible by 3 that is greater than 50")
+    else:
+        print("positive, >50")
+
+elif count < 0: #negative number
+    print("negative")
+
+elif count == 50:
+    print("50")
+    
+else: #zero
+    print("number is zero")
