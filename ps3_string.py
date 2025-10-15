42206e43653e404d664b3effe453198e7fe31840
Jane M. Liu <jane.liu@pomona.edu>
2025-10-15 11:33:03 -0400

    counted the numbers of A C G T

--- ps3_string.py
+++ ps3_string.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import sys
 
-dna_seq = sys.argv[1] #this allows me to paste in the sequence when running the command
+dna_orig = sys.argv[1] #this allows me to paste in the sequence when running the command
 
-print(dna_seq)
+dna_seq = dna_orig.upper() #this converts the sequence to uppercase
+
+print(f"The number of A's, C's, G's, and T's in my sequence are {
+dna_seq.count('A')}, {dna_seq.count('C')}, {dna_seq.count('G')}, and {dna_seq.count('T')}, respectively.")
