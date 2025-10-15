9c902855d3ae5d916ab69854a8cbb5b2af7754eb
Jane M. Liu <jane.liu@pomona.edu>
2025-10-15 19:37:58 -0400

    through problem 5

--- ps4_loops.py
+++ ps4_loops.py
@@ -7,4 +7,21 @@ while count < 101:
     count+=1
 print("Done")
 
- #this should pring out the numbers 1 to 100
+ #this should bring out the numbers 1 to 100
+
+count = 0
+count_sum = 0
+while count < 100:
+    count = count + 1
+    count_sum = count_sum + count
+
+print(count_sum)
+
+#this sums the numbers between 0 and 100
+
+count = 11
+factorial = 1
+while count > 1:
+    count = count - 1
+    factorial = factorial * count
+print(factorial)
