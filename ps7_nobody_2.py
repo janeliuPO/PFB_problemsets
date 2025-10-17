742c8c333ce7d77e484af138684a1d99570b5957
Jane M. Liu <jane.liu@pomona.edu>
2025-10-17 12:09:18 -0400

    substituted and wrote to file

--- ps7_nobody_2.py
+++ ps7_nobody_2.py
@@ -6,6 +6,9 @@ import re
 shel = open("Python_07_nobody.txt", "r")
 contents = shel.read()
 
-#use finditer to find position of the subpattern matches
-for found in re.finditer(r"Nobody", contents):
-    print("position:" , found.start(), found.end(), sep="\t")
+#use re.sub to substitute Greg for Nobody
+new_contents = re.sub(r"Nobody", r"Greg", contents)
+print(contents)
+print(new_contents)        
+with open("Python_07_Greg.txt", "w") as file_object:
+    file_object.write(new_contents)                                          
