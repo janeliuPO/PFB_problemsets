88450b19b2c797bb8d32b10891cfa96bb5be7d2b
Jane M. Liu <jane.liu@pomona.edu>
2025-10-17 17:08:31 -0400

    found restriction sites and marked them in the sequence

---
+++ ps7_ApoI.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import re
+fastaDict_re = {}
+
+#open and read the text line by line
+with open("Python_07_ApoI.fasta","r") as fasta_read:
+    for line in fasta_read:
+        line = line.rstrip()
+        if re.search(r">", line):
+            # print(line)
+            for found in re.finditer(r">(\S+.*)", line):
+                seqID = found.group(1)
+            # print(seqID)
+            fastaDict_re[seqID] = ''
+        else: 
+            fastaDict_re[seqID] += line
+
+# print(fastaDict_re)
+
+new_fasta = re.sub(r"([AG])(AATT[CT])", r"\1^\2", fastaDict_re['seq1'])
+print(new_fasta)
