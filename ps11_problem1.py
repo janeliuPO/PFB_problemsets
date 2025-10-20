b1c74092bbbebc27236aafa52621901b892ae650
Jane M. Liu <jane.liu@pomona.edu>
2025-10-20 16:35:29 -0400

    did the GC content problem

--- ps11_problem1.py
+++ ps11_problem1.py
@@ -22,8 +22,18 @@ def dna_counts(dna):
     count_T = seq.count('T')
     count_C = seq.count('C')
     count_G = seq.count('G')
-    counts = f'the number of As is: {count_A}, the number of Ts is: {count_T}, the number of Cs is: {count_C}, and the number of Ts is: {count_T}.'
+    counts = f'the number of As is: {count_A}, the number of Ts is: {count_T}, the number of Cs is: {count_C}, and the number of Gs is: {count_G}.'
     return counts
+
+def dna_gc(dna):
+    clean_dna = re.sub(r"\s+", "", dna) 
+    #replace all non-characters with nothing
+    seq = clean_dna.upper()
+    count_C = seq.count('C')
+    count_G = seq.count('G')
+    total_nt = len(clean_dna)
+    gc_content = (count_G + count_C)/(total_nt)
+    return gc_content
 
 dna_seq1 = DNASequence('agagagtttccc', 'test1', 'bunnies')
 dna_seq2 = DNASequence('aaggttaaggtt', 'test2', 'panda')
@@ -31,3 +41,4 @@ dna_seq2 = DNASequence('aaggttaaggtt', 'test2', 'panda')
 for input in [dna_seq1,dna_seq2]:
     print(f'{input.seq_name}({input.organism}) has the sequence: {input.sequence} with a length of {dna_length(input.sequence)}.')
     print(f'For {input.seq_name}, {dna_counts(input.sequence)}')
+    print(f'The GC content is {dna_gc(input.sequence):.2%}')
