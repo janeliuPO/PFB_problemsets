6eb489374e4d2bf86e92f6138b7f32fffc14713d
Jane M. Liu <jane.liu@pomona.edu>
2025-10-20 16:07:07 -0400

    did the first class problem!

--- ps11_problem1.py
+++ ps11_problem1.py
@@ -2,8 +2,15 @@
 import sys
 import re
 
-def dna_rc(dna):
-    clean_dna = re.sub(r"\s+", "", dna) 
+class DNASequence(object):
+
+    def __init__(self, sequence, seq_name, organism):
+        self.sequence = sequence.upper()
+        self.seq_name = seq_name
+        self.organism = organism
+    
+def dna_rc(self):
+    clean_dna = re.sub(r"\s+", "", self) 
     #replace all non-characters with nothing
     seq = clean_dna.upper()
     seq = seq.replace('A', 't')
@@ -13,5 +20,8 @@ def dna_rc(dna):
     seq = seq[::-1]
     return seq.upper()
 
-dna = 'GGA aat CC\n'
-print(f'The reverse complement of {dna} is {dna_rc(dna)}.')
+dna_seq1 = DNASequence('agagagtttccc', 'test1', 'bunnies')
+dna_seq2 = DNASequence('aaggttaaggtt', 'test2', 'panda')
+
+for input in [dna_seq1,dna_seq2]:
+    print(f'{input.seq_name} has the sequence: {input.sequence} and is from {input.organism}.')
