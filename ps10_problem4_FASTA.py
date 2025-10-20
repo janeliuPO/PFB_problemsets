0cedab1e22530daa9f3cd8456e5a153e59b545e2
Jane M. Liu <jane.liu@pomona.edu>
2025-10-20 13:44:18 -0400

    got the FASTA writer made

--- ps10_problem4_FASTA.py
+++ ps10_problem4_FASTA.py
@@ -4,42 +4,28 @@ import re
 
 def dna_num(file_read):
     fastaDict = {}
+    width = int(sys.argv[2])
     #open from user input and read the text line by line
     with open(sys.argv[1],"r") as file_read:
         for line in file_read:
             line = line.rstrip()
             if line.find('>') == 0:
                 seqID = line
-                #does key already exist in the dictionary
             else:
-                if seqID in fastaDict:
-                    seq = fastaDict[seqID] + line
-                    fastaDict[seqID] = seq
-                else: 
-                    fastaDict[seqID] = line
                 chunks = []
-                for key in fastaDict:
-                    value = fastaDict[key]
-                    for i in range(0, len(value), width):
-                        chunks.append(value[i:i+width])
-                    fasta = f'{seqID}\n{chunks}\n'
-
-    # chunks = []
-    # for i in range(0, len(clean_dna), width):
-    #     chunks.append(clean_dna[i:i+width])
-    #if printing to Python can use \n
-    #if writing to text file, then \n will be saved
-    #for i in range(0, len(clean_dna), width) = iterating through the string
-    #or list clean_dna in fixed-size steps, defined by the variable width. 
-    #start = 0, starting index
-    #stop = len(clean_dna) is ending index of the sequence
-    #step = width
-    return fasta
+                for i in range(0, len(line), width):
+                    chunks.append(line[i:i+width])
+                if seqID not in fastaDict:
+                    fastaDict[seqID] = chunks
+                else: 
+                    fastaDict[seqID].append(chunks)
+    fasta_file = "" #anytime want a string, list, etc to be added to,
+    #have outside of loop
+    for seqID, chunks in fastaDict.items():    
+        fasta_file += f'{seqID}\n{"\n".join(chunks)}\n' 
+    return fasta_file
 
 file_read = sys.argv[1]
-width = 5
-print(dna_num(file_read))
-# dna_length = [len(s) for s in dna_num(dna, width)]
-# print(dna_length)
-# for sequence in dna_list(dna):
-#     print(sequence)
+width = sys.argv[2]
+fo = open("Python_10.output.fa", "w")
+fo.write(dna_num(file_read))
