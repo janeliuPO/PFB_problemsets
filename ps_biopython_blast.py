e3aa48b0a2cb97aceeaa8c7fc588c8c456e61392
Jane M. Liu <jane.liu@pomona.edu>
2025-10-21 21:03:52 -0400

    did the first bast match problem!

--- ps_biopython_blast.py
+++ ps_biopython_blast.py
@@ -1,48 +1,30 @@
 #!/usr/bin/env python3
 import sys
+import re
 from Bio import SeqIO
 
 filename = sys.argv[1]
 fasta_dict = SeqIO.to_dict(SeqIO.parse(filename, "fasta"))
 total_seqs = len(fasta_dict)
+print(f'total sequences: {total_seqs}')
 # print(fasta_dict)
-print(fasta_dict)
+
+substring = 'Salmonella paratyphi B'
+matching_items = {}
 
 for seq_record in SeqIO.parse(filename, "fasta"):
-    gene_id = seq_record.id
+    description = seq_record.description
     sequence = seq_record.seq
-    organism = seq_record.org
-    print(gene_id)
-    print(sequence)
-    print(organism)
-
-# search_string = 'Salmonella paratyphi B'
-# spb_dict = {}
-# for key, value in fasta_dict.items():
-#     if search_string in value:
-#         spb_dict[key] = value
-
-# print(spb_dict)
-
-# for seq_record in SeqIO.parse(filename, "fasta"):
-#     gene_id = seq_record.id
-#     sequence = seq_record.seq
-    # aa_count += len(sequence)
+    gene_id = seq_record.id
+    # print(description)
     # print(sequence)
+    # print(gene_id)
+    for found in re.findall(r"Salmonella paratyphi B", description):
+        matching_items[gene_id] = sequence
 
+print(matching_items)
 
-# with open("ps_blast.out", "w") as fasta_file:
-#     fasta_file.write(fasta_dict[0])
-# # print(sorted_list)
-# print(f'total number of nucleotides: {nt_count}')
-# print(f'average length of sequences: {nt_count/total_seqs}')
-# print(f'shortest sequence length: {len(sorted_list[0])}')
-# print(f'longest sequence length: {len(sorted_list[-1])}')
-# print(f'average gc content of sequences: {gc_pct:.2%}')
-# sorted_gc = sorted(gc_dict.items(), key=lambda item: item[1])
-# smallest_key, smallest_value = sorted_gc[0]
-# largest_key, largest_value = sorted_gc[-1]
-# print(f'lowest GC content: {smallest_key}: {smallest_value:.2%}')
-
-# print(f'highest GC content: {largest_key}: {largest_value:.2%}')
+with open("s_paratyphi.prot.fa", "w") as file:
+    for key, value in matching_items.items():
+        file.write(f'>{key}\n{value}\n')
 
