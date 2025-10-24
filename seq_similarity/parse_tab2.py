1825d7d7a1f952be28a1d2d03c8e43e8efd22eb1
Jane M. Liu <jane.liu@pomona.edu>
2025-10-24 09:25:20 -0400

    cleaning

--- seq_similarity/parse_tab2.py
+++ seq_similarity/parse_tab2.py
@@ -1,18 +1,6 @@
 
 #!/usr/bin/env python3
 
-# To download a set of results files automatically:
-#     ## for ssearch results:
-#     for m in BL50 BP62 VT10 VT20 VT40 VT80 VT160; do
-#     curl -O https://fasta.bioch.virginia.edu/mol_evol/data/ss_rand5-200_v_qfo_${m}.txt
-#     curl -O https://fasta.bioch.virginia.edu/mol_evol/data/ss_rand5-800_v_qfo_${m}.txt
-#     done
-
-#     ## for BLASTP results:
-#     for m in BLOSUM62 BLOSUM80 PAM30 PAM70; do
-#     curl -O https://fasta.bioch.virginia.edu/mol_evol/data/blast_rand5-200_v_qfo_${m}.txt
-#     curl -O https://fasta.bioch.virginia.edu/mol_evol/data/blast_rand5-800_v_qfo_${m}.txt
-#     done
 
 import sys
 
